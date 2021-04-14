148eab64f291584531cfa9c88fa602fa5fd21334
Jacob Odero <jackjax617@gmail.com>
2021-04-14 20:57:34 +0300

    add reports and new database

--- petshop_mgt.sql
+++ petshop_mgt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2021 at 10:27 AM
+-- Generation Time: Apr 14, 2021 at 07:56 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `petshop_mgt`
 --
-CREATE DATABASE IF NOT EXISTS `petshop_mgt` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `petshop_mgt`;
 
 -- --------------------------------------------------------
 
@@ -30,10 +28,19 @@ USE `petshop_mgt`;
 --
 
 CREATE TABLE `adopted` (
-  `id` int(11) NOT NULL,
   `pet_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `addoption_date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `adopted`
+--
+
+INSERT INTO `adopted` (`pet_id`, `user_id`, `addoption_date`) VALUES
+(20, 6, '2021-04-14'),
+(14, 6, '2021-04-14'),
+(13, 6, '2021-04-14');
 
 -- --------------------------------------------------------
 
@@ -42,7 +49,6 @@ CREATE TABLE `adopted` (
 --
 
 CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
   `c_id` int(11) NOT NULL,
   `c_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -51,12 +57,12 @@ CREATE TABLE `category` (
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`id`, `c_id`, `c_name`) VALUES
-(1, 1, 'Dog'),
-(2, 2, 'Cat'),
-(3, 3, 'Fish'),
-(4, 4, 'Bird'),
-(5, 5, 'Other');
+INSERT INTO `category` (`c_id`, `c_name`) VALUES
+(1, 'Dog'),
+(2, 'Cat'),
+(3, 'Fish'),
+(4, 'Bird'),
+(5, 'Other');
 
 -- --------------------------------------------------------
 
@@ -87,11 +93,12 @@ INSERT INTO `pets` (`id`, `pet_name`, `category_id`, `age`, `pet_description`, `
 (10, 'Dracula', 2, 18, 'Dracula is a ginger cat', '2021-03-12 00:00:00', 4, NULL, 0),
 (11, 'Jolly', 1, 21, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, NULL, 0),
 (12, 'Simba', 2, 0, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, NULL, 0),
-(13, 'Mekka', 3, 15, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, NULL, 0),
-(14, 'Bishop', 1, 12, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, NULL, 0),
+(13, 'Mekka', 3, 15, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, 6, 1),
+(14, 'Bishop', 1, 12, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, 6, 1),
 (15, 'Nuke', 5, 5, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, NULL, 0),
 (16, 'Fenny', 4, 13, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, NULL, 0),
-(17, 'Perkí', 4, 11, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, NULL, 0);
+(17, 'Perkí', 4, 11, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release', '2021-03-16 10:21:20', 1, NULL, 0),
+(20, 'Laduuma', 4, 17, 'When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you', '2021-04-14 18:57:24', 6, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -118,7 +125,8 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `phon
 (1, 'Pet', 'Shop', 'Pet Shop', 'petshop@pestshop.com', '0202242525', 'Ongata Rongai', '$2y$10$GpNCkle45q67yOwEW7hWM.bgpM5dyy4zVsPKibqec7S64FXbO4uUa\r\n'),
 (2, 'Chandler', 'Bing', 'Mr Big', 'djack@test.com', '0729170437', 'Nairobi, Kenya', '$2y$10$OZo3Fl1Nj72SadnHoaO03erb8uoOwqTG4mJSnasjb8kL9SYI6zROu'),
 (3, 'des', 'mond', 'mondi', 'sam@test.com', '0712345698', 'Nairobi, Kenya', '$2y$10$EbUTIc87EsAOg4dc2Y9l7e5yhEOYXdZ3brhqy.LV3tuEloMgGrOWa'),
-(4, 'new', 'person', 'newP', 'Np@email.com', '0798990989', 'Mukuru, Kenya', '$2y$10$Qt2vj1smZ7nFcz9s.hUNsePSf.xFzBhrRiobZbmw164Ip38XIRLqq');
+(4, 'new', 'person', 'newP', 'Np@email.com', '0798990989', 'Mukuru, Kenya', '$2y$10$Qt2vj1smZ7nFcz9s.hUNsePSf.xFzBhrRiobZbmw164Ip38XIRLqq'),
+(6, 'Allan', 'Bing', 'Kondoo wa kutupa', 'test003@flasktest.com', '0765453678', 'Nairobi, Kenya', '$2y$10$l.24qiG.fv6P2ZT2qyZ8Ru6L0vTjDwSOwbwxO41kF/XfY2ZqAZojq');
 
 --
 -- Indexes for dumped tables
@@ -128,7 +136,6 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `phon
 -- Indexes for table `adopted`
 --
 ALTER TABLE `adopted`
-  ADD PRIMARY KEY (`id`),
   ADD KEY `pet_id` (`pet_id`,`user_id`),
   ADD KEY `user_id` (`user_id`);
 
@@ -136,7 +143,7 @@ ALTER TABLE `adopted`
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`c_id`),
   ADD KEY `c_id` (`c_id`);
 
 --
@@ -159,28 +166,16 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `adopted`
---
-ALTER TABLE `adopted`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- AUTO_INCREMENT for table `pets`
 --
 ALTER TABLE `pets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -197,9 +192,9 @@ ALTER TABLE `adopted`
 -- Constraints for table `pets`
 --
 ALTER TABLE `pets`
-  ADD CONSTRAINT `pets_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
   ADD CONSTRAINT `pets_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pets_ibfk_3` FOREIGN KEY (`buyer_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `pets_ibfk_3` FOREIGN KEY (`buyer_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pets_ibfk_4` FOREIGN KEY (`category_id`) REFERENCES `category` (`c_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
